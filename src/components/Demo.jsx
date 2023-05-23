641ee106fc5b8d2bab3179409e8f739a04219a04
anjali-sim <anjalighetia123@gmail.com>
2023-05-23 17:55:13 +0530

    :hammer:Created a component

---
+++ src/components/Demo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Demo = () => {
+    const apiUrl = import.meta.env.VITE_API_URL;
+console.log(apiUrl);
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default Demo
